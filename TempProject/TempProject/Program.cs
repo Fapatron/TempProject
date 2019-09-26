53eb871dac1465343dcbf6fbf8b383fda4c7d049
Fapatron <55849320+Fapatron@users.noreply.github.com>
2019-09-26 21:09:42 +0300

    Nauja range FX
    Nauja range FX

---
+++ TempProject/TempProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Irasyk tris skaicisu");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int max = Max(a, b, c);
+            Console.WriteLine("Didziausias skaicisu: " + max);
+
+            Console.ReadLine();
+        }
+        static int Max(int a, int b, int c)
+        {
+            if(a > b && a > c)
+            {
+                return a;
+            }
+            else if(b > a && b > c)
+            {
+                return b;
+            }
+            else //if (c > a || c > b)
+            {
+                return c;
+            }
+            //return 0;
+        }
+        static int InRange(int a, int b, int c)
+        {
+            if (a >= b && a <= c)
+            {
+                //string return
+            }
+            return 0;
+        }
+
+
+    }
+}
+
